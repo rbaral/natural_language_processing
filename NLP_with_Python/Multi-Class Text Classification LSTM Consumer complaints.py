3ad90febda8ee41b47fde9ee9ff3813699aa218f
Ramesh Baral <ramesh.baral@tivo.com>
2021-04-28 14:19:16 -0400

    additional works related to NLP in python

---
+++ NLP_with_Python/Multi-Class Text Classification LSTM Consumer complaints.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import EarlyStopping
+from keras.layers import Dropout
+import re
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+STOPWORDS = set(stopwords.words('english'))
+from bs4 import BeautifulSoup
+import plotly.graph_objs as go
+import plotly.plotly as py
+import cufflinks
+from IPython.core.interactiveshell import InteractiveShell
+import plotly.figure_factory as ff
+InteractiveShell.ast_node_interactivity = 'all'
+from plotly.offline import iplot
+cufflinks.go_offline()
+cufflinks.set_config_file(world_readable=True, theme='pearl')
+
+
+# In[2]:
+
+
+df = pd.read_csv('consumer_complaints_small.csv')
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+df.info()
+
+
+# In[5]:
+
+
+df.Product.value_counts()
+
+
+# In[6]:
+
+
+df.loc[df['Product'] == 'Credit reporting', 'Product'] = 'Credit reporting, credit repair services, or other personal consumer reports'
+df.loc[df['Product'] == 'Credit card', 'Product'] = 'Credit card or prepaid card'
+df.loc[df['Product'] == 'Payday loan', 'Product'] = 'Payday loan, title loan, or personal loan'
+df.loc[df['Product'] == 'Virtual currency', 'Product'] = 'Money transfer, virtual currency, or money service'
+df = df[df.Product != 'Other financial service']
+
+
+# In[7]:
+
+
+df['Product'].value_counts().sort_values(ascending=False).iplot(kind='bar', yTitle='Number of Complaints', 
+                                                                title='Number complaints in each product')
+
+
+# In[8]:
+
+
+def print_plot(index):
+    example = df[df.index == index][['Consumer complaint narrative', 'Product']].values[0]
+    if len(example) > 0:
+        print(example[0])
+        print('Product:', example[1])
+
+
+# In[9]:
+
+
+print_plot(10)
+
+
+# In[10]:
+
+
+print_plot(100)
+
+
+# In[11]:
+
+
+df = df.reset_index(drop=True)
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+STOPWORDS = set(stopwords.words('english'))
+
+def clean_text(text):
+    """
+        text: a string
+        
+        return: modified initial string
+    """
+    text = text.lower() # lowercase text
+    text = REPLACE_BY_SPACE_RE.sub(' ', text) # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
+    text = BAD_SYMBOLS_RE.sub('', text) # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing. 
+    text = text.replace('x', '')
+#    text = re.sub(r'\W+', '', text)
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS) # remove stopwors from text
+    return text
+df['Consumer complaint narrative'] = df['Consumer complaint narrative'].apply(clean_text)
+
+
+# In[12]:
+
+
+df['Consumer complaint narrative'] = df['Consumer complaint narrative'].str.replace('\d+', '')
+
+
+# In[13]:
+
+
+print_plot(10)
+
+
+# In[14]:
+
+
+print_plot(100)
+
+
+# In[15]:
+
+
+# The maximum number of words to be used. (most frequent)
+MAX_NB_WORDS = 50000
+# Max number of words in each complaint.
+MAX_SEQUENCE_LENGTH = 250
+# This is fixed.
+EMBEDDING_DIM = 100
+
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+tokenizer.fit_on_texts(df['Consumer complaint narrative'].values)
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+
+# In[16]:
+
+
+X = tokenizer.texts_to_sequences(df['Consumer complaint narrative'].values)
+X = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
+print('Shape of data tensor:', X.shape)
+
+
+# In[17]:
+
+
+Y = pd.get_dummies(df['Product']).values
+print('Shape of label tensor:', Y.shape)
+
+
+# In[18]:
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.10, random_state = 42)
+print(X_train.shape,Y_train.shape)
+print(X_test.shape,Y_test.shape)
+
+
+# In[20]:
+
+
+model = Sequential()
+model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
+model.add(SpatialDropout1D(0.2))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(13, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+
+# In[21]:
+
+
+epochs = 5
+batch_size = 64
+
+history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,validation_split=0.1,callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+
+
+# In[22]:
+
+
+accr = model.evaluate(X_test,Y_test)
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
+
+
+# In[23]:
+
+
+plt.title('Loss')
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
+plt.show();
+
+
+# In[24]:
+
+
+plt.title('Accuracy')
+plt.plot(history.history['acc'], label='train')
+plt.plot(history.history['val_acc'], label='test')
+plt.legend()
+plt.show();
+
+
+# Test with a new complaint.
+
+# In[39]:
+
+
+new_complaint = ['I am a victim of identity theft and someone stole my identity and personal information to open up a Visa credit card account with Bank of America. The following Bank of America Visa credit card account do not belong to me : XXXX.']
+seq = tokenizer.texts_to_sequences(new_complaint)
+padded = pad_sequences(seq, maxlen=MAX_SEQUENCE_LENGTH)
+pred = model.predict(padded)
+labels = ['Credit reporting, credit repair services, or other personal consumer reports', 'Debt collection', 'Mortgage', 'Credit card or prepaid card', 'Student loan', 'Bank account or service', 'Checking or savings account', 'Consumer Loan', 'Payday loan, title loan, or personal loan', 'Vehicle loan or lease', 'Money transfer, virtual currency, or money service', 'Money transfers', 'Prepaid card']
+print(pred, labels[np.argmax(pred)])
+
+
+# In[ ]:
+
+
+
+
